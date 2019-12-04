5b519d17ef67accf57f169904de69ef2137f9c3c
Vladimir Vásquez Hernández <ext_vvazqueh@mercadolibre.com>
2019-12-04 08:13:03 -0500

    Test with input data set

--- app/main.go
+++ app/main.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println(PrintMessage())
+	fmt.Println(Sum(2,3))
 }
 
 func PrintMessage() string {
 	return "Hello World"
+}
+
+func Sum(x, y int) int {
+	return x + y
 }
--- app/main_test.go
+++ app/main_test.go
@@ -6,11 +6,31 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func shouldReturnAMessageUsingTestify(t *testing.T) {
+func TestShouldReturnAMessageUsingTestify(t *testing.T) {
 	assert := assert.New(t)
 
 	var expected string = "Hello World"
 	var current = PrintMessage()
 
 	assert.Equal(expected, current, "The two messages should be the same.")
+	assert.NotNil(current)
+}
+
+func TestShouldReturnASumUsingTestify(t *testing.T) {
+	assert := assert.New(t)
+
+	var tests = []struct {
+		x           int
+		y           int
+		expectedSum int
+	}{
+		{2, 3, 5},
+		{-1, 1, 0},
+		{0, 10, 10},
+		{-5, -3, -8},
+	}
+
+	for _, test := range tests {
+		assert.Equal(Sum(test.x, test.y), test.expectedSum)
+	}
 }
