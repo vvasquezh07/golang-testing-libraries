219f151727daee0d7312c973402c50455d4569ff
Vladimir Vásquez Hernández <ext_vvazqueh@mercadolibre.com>
2019-12-11 09:13:19 -0500

    Adding a pending test

--- app/ginkgo/books/book_test.go
+++ app/ginkgo/books/book_test.go
@@ -9,35 +9,45 @@ import (
 
 var _ = Describe("Book", func() {
 	var (
-        longBook  Book
-        shortBook Book
-    )
+		longBook  Book
+		shortBook Book
+	)
 
-    BeforeEach(func() {
-        longBook = Book{
-            Title:  "Les Miserables",
-            Author: "Victor Hugo",
-            Pages:  1488,
-        }
+	BeforeEach(func() {
+		longBook = Book{
+			Title:  "Les Miserables",
+			Author: "Victor Hugo",
+			Pages:  1488,
+		}
 
-        shortBook = Book{
-            Title:  "Fox In Socks",
-            Author: "Dr. Seuss",
-            Pages:  24,
-        }
-    })
+		shortBook = Book{
+			Title:  "Fox In Socks",
+			Author: "Dr. Seuss",
+			Pages:  24,
+		}
+	})
 
-    Describe("Categorizing book length", func() {
-        Context("With more than 300 pages", func() {
-            It("should be a novel", func() {
+	Describe("Categorizing book length", func() {
+		Context("With more than 300 pages", func() {
+			It("should be a novel", func() {
                 Expect(longBook.CategoryByLength()).To(Equal("NOVEL"))
-            })
-        })
+			})
+		})
 
-        Context("With fewer than 300 pages", func() {
-            It("should be a short story", func() {
-                Expect(shortBook.CategoryByLength()).To(Equal("SHORT STORY"))
-            })
-        })
-    })
+		Context("With fewer than 300 pages", func() {
+			It("should be a short story", func() {
+				Expect(shortBook.CategoryByLength()).To(Equal("SHORT STORY"))
+			})
+		})
+	})
+
+	Describe("Obtaining the title", func() {
+		It("should be les miserables", func() {
+			Expect(longBook.Title).To(Equal("Les Miserables"))
+		})
+
+		PIt("should be la hojarasca", func() {
+			Expect(shortBook.Title).To(Equal("La hojarasca"))
+		})
+	})
 })
